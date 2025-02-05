d06cb66f456b2403b178cae988a068e090c3cf35
Murciana <murciana.lb@gmail.com>
2025-02-05 11:53:20 +0100

    ajout des views 'about' et 'projects'/ about bien avancé

--- src/app/p9/p9.component.ts
+++ src/app/p9/p9.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@ngbracket/ngx-layout';
 
 @Component({
   selector: 'app-p9',
   standalone: true,
-  imports: [FlexLayoutModule],
+  imports: [FlexLayoutModule, CommonModule],
   templateUrl: './p9.component.html',
   styleUrl: './p9.component.scss'
 })
 export class P9Component  {
+  view: string = 'home';
 
+  handleView(selectedView: string) {
+    this.view = selectedView;
+  }
 }
