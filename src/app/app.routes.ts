74656995fb0d00a803080504bd58794be257daa3
Murciana <murciana.lb@gmail.com>
2025-02-20 11:09:26 +0100

    composants générés pour réorganiser éléments de p9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { P9Component } from './p9/p9.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ArticlesComponent } from './articles/articles.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: P9Component},
+  {path: 'p9', component: P9Component},
+  {path: 'home', component: HomeComponent},
+  {path: 'about' , component: AboutComponent},
+  {path: 'projects' , component: ProjectsComponent},
+  {path: 'articles' , component: ArticlesComponent}
+];
